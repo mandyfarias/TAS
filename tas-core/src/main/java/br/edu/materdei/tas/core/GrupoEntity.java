30c23a730563666211743c7f5b676fceac8fb900
Mandy <Mandy@DESKTOP-O8V6VKO>
2020-08-26 19:27:42 -0300

    Aula 19/08/2020

---
+++ tas-core/src/main/java/br/edu/materdei/tas/core/GrupoEntity.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.materdei.tas.core;
+
+/**
+ *
+ * @author Mandy
+ */
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+
+
+@Entity
+@Table(name="grupo")
+public class GrupoEntity {
+    @Id
+    @GeneratedValue
+    
+    private Integer id;
+    private String nome;
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+        
+}
